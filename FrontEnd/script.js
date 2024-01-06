53356c285a41a52ceae69ed94065c7d1a0706b57
SamBen16 <adresse123456789101112@gmail.com>
2024-01-06 19:58:26 +0100

    ajout de boutons filtres pour afficher les travaux par catégorie

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,28 +1,77 @@
 fetch('http://localhost:5678/api/works')
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    for (let i = 0; i < data.length ; i++) {
-        console.log(data[i].imageUrl);
-        console.log(data[i].title);
-        // console.log(data[i].category.name);
-        // Récupération de la classe gallery
-        const gallery = document.querySelector('.gallery');
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        // Boucler le nombre de figures sur la page d'accueil
+        for (let i = 0; i < data.length; i++) {
+            // Récupération de la classe gallery
+            const gallery = document.querySelector('.gallery');
 
-        // Création des éléments 
-        const figure = document.createElement('figure');
-        const img = document.createElement('img');
-        const figcaption = document.createElement('figcaption');
-        
-        // Intégration du contenu dans chaque élément
-        img.src = data[i].imageUrl;
-        img.alt = data[i].title;
-        figcaption.innerText = data[i].title;
+            // Création des éléments 
+            const figure = document.createElement('figure');
+            const img = document.createElement('img');
+            const figcaption = document.createElement('figcaption');
+            
+            // Intégration du contenu dans chaque élément
+            img.src = data[i].imageUrl;
+            img.alt = data[i].title;
+            figcaption.innerText = data[i].title;
 
-        // Intégration au niveau du DOM
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        gallery.appendChild(figure);
-    }
-})
-.catch(console.error);
+            // Intégration au niveau du DOM
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+            gallery.appendChild(figure);
+
+            // Utilisation de la fonction pour récupérer la catégorie
+            let category = getCategory(data[i]);
+            figure.setAttribute('id', category);
+        }
+
+        // Partie boutons et filtres
+        const categories = new Set();
+        data.forEach(element => {
+            let category = getCategory(element);
+            console.log(category);
+            categories.add(category);
+        });
+        console.log(categories);
+
+        const buttons = document.querySelector('.buttons');
+
+        // intégration du bouton "tous"
+        const buttonTous = document.createElement('button');
+        buttonTous.setAttribute("type", "button");
+        buttonTous.setAttribute("id", "tous");
+        buttonTous.innerText = "tous";
+        buttons.appendChild(buttonTous);
+        buttonTous.addEventListener('click', function() {
+            const gallery = document.querySelector('.gallery');
+            const figures = gallery.querySelectorAll('figure');
+            figures.forEach(figure => {
+                figure.style.display = "block";
+            });
+        });
+
+        // intégration des boutons pour chaque catégorie
+        categories.forEach(category => {
+            const button = document.createElement('button');
+            button.setAttribute('type', 'button');
+            button.setAttribute('id', category);
+            button.innerText = category;
+            buttons.appendChild(button);
+            button.addEventListener('click', function() {
+                const gallery = document.querySelector('.gallery');
+                const figures = gallery.querySelectorAll('figure');
+                figures.forEach(figure => {
+                    figure.style.display = (figure.getAttribute('id') === category || category === "tous") ? "block" : "none";
+                });
+                
+            });
+        });
+    })
+    .catch(console.error);
+
+// Fonction pour récupérer la catégorie d'un élément
+function getCategory(element) {
+    return element.category.name;
+}
